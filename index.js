91e5490c0791ed7fb59dcef0f2a93986a305e84b
Markus Corvinus <markus.corvinus0827@gmail.com>
2023-11-06 18:59:22 +0800

    after monday class

--- index.js
+++ index.js
@@ -1,47 +1,165 @@
 //Variable Declaration
 
 //debugging
+let firstName = "John Doe";
+let age = 24;
+let address = "Cavite";
+let ageOfPerson = "24";
+//console.log("Hello world " + firstName + ", i'm "+ age + "years old. I live in "+ address);
+//console.log(`Hello world ${firstName}, i'm ${age} years old. I live in ${address}`);
 
 //1. DATA TYPES
 
     //String
+        let dataType1 = "John Doe";
+        //console.log(dataType1);
+        dataType1 = "John Davis";
+        //console.log(dataType1);
+
         //length
-        //replace  
+        //console.log(dataType1.length);
+        //replace      0123456789
+        let strEmail = "john_cena@gmail.com";
+        let customEmail = strEmail.replace("@gmail.com","@sdca.edu.ph");
+
+        //console.log(`before: ${strEmail}, after ${customEmail}`);
         //indexOf
+        let posString = strEmail.indexOf("@");
+        //console.log(posString);
         //slice
+        let sliceString = strEmail.slice(5,9);
+        //console.log(sliceString);
         //substr
+        let strString = strEmail.substr(5,9);
+        //console.log(strString);
         //Autosuggest email;
 
+        let setEmail = "helllowoeajkdjsa@test.edu.com";
+        //you have an email input, return the domain name of email from the email input.
+        //let maxString = ;
+        let emailDom = setEmail.slice(setEmail.indexOf("@"));
+        //console.log(emailDom);
 
     //Integer
+        
+        let ageSample = ageOfPerson; 
+        //console.log(24+parseFloat(ageSample));
+        let numInput = 57;
+        
+        // if(numInput % 2){
+        //     console.log("Odd");
+        // } else {
+        //     console.log("Even");
+        // }
         //parseInt
         //parseFloat
         //Number
         //modulo
 
     //Boolean
-
+        let isAdmin = true;
+        let isLoggin = true;
+        let isPaid = true;
+        let strPaid = "true";
+        //console.log(typeof isPaid);
     //Null
-
+        let undefVar = null;
     //Undefined
-
+        let undefVar2;
+        //console.log(undefVar2);
     //Symbol
-
+        const fixedValue = Symbol("Hello");
+        //console.log(fixedValue);
+        //fixedValue = Symbol("New Value");
     //BigInt
 
-    //Array 
+    //typeOf    
+
+    //Array                 0       1   2       3       4         5         6          7   
+        let userData1 = ["john doe",24,false,"Cavite"];
+        let userData2 = ["john davis",20,true,"Cavite"];
+
+        //console.log(userData[1]);
         //length
+        //console.log(userData1.length);
+
         //indexOf
+        //console.log(userData1.indexOf("1998"));
+
         //concat
+        let combineUser = userData1.concat(userData2);
+        //console.log(userData1);
+
+        //push 
+        //console.log(userData1.push("1998",100,"Luzon"));
+        //console.table(userData1);
+
+        //pop
+        // userData1.pop();
+        // console.table(userData1);
+
+        //shift
+        // userData1.shift();
+        // console.table(userData1);
+
         //include
+        console.log(userData1.includes("Cavite2"));
         
     //Object
+        let userDataObj = {
+            firstname: "John",
+            lastname: "Doe",
+            age: 24,
+            isEnrolled: false,
+            address: "Cavite"
+        }
 
+        let keyObj = "address"; 
+        // console.log(userDataObj.lastname);    //dot-readability   
+        // console.log(userDataObj[keyObj]); //bracket much flexible
+        //console.table(userDataObj);
     //Date
+        const todayDate = new Date();
+        ///console.log(todayDate.toDateString()); //"YYYY-MM-DD"
+        const currentYear   = todayDate.getFullYear();
+        const currentMonth  = todayDate.getMonth() + 1;
+        const currentDay    = todayDate.getDate();
+
+        const fullDate = currentYear+"-"+currentMonth+"-"+currentDay;
+        //console.log(fullDate);
+        
 //2. OPERATION AND EXPRESSION
     //equal
+    let password = "admin";
+    let inputpassword = "";
+    //let inputpassword = prompt("Type your password");
+    
+    
+    // if(password == inputpassword){
+    //     console.log("success");
+    // } else {
+    //     console.log("Wrong Password");
+    // }
     //strict equal
+    let numberVal = 250;
+    let stringVal = "250";
+
+    if(numberVal === stringVal){
+        console.log("same amount");
+    } else {
+        console.log("not same amount");
+    }
+
     //not equal
+    let isAdmin2 = "admin";
+   
+
+    if(isAdmin2 != "admin"){
+        console.log("You not an admin");
+    } else {
+        console.log("You are admin");
+    }
+
     //greater than
     //greater than or equal
     //less than
