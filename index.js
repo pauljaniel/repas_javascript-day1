cf06bda921872fe6426174a9b29c34f2c8fe178c
Miko Babiano <miko.gaming1975@gmail.com>
2023-11-06 14:27:47 +0800

    based code for 2023-11-06

---
+++ index.js
@@ -0,0 +1,103 @@
+//Variable Declaration
+
+//debugging
+
+//1. DATA TYPES
+
+    //String
+        //length
+        //replace  
+        //indexOf
+        //slice
+        //substr
+        //Autosuggest email;
+
+
+    //Integer
+        //parseInt
+        //parseFloat
+        //Number
+        //modulo
+
+    //Boolean
+
+    //Null
+
+    //Undefined
+
+    //Symbol
+
+    //BigInt
+
+    //Array 
+        //length
+        //indexOf
+        //concat
+        //include
+        
+    //Object
+
+    //Date
+//2. OPERATION AND EXPRESSION
+    //equal
+    //strict equal
+    //not equal
+    //greater than
+    //greater than or equal
+    //less than
+    //less than or equal
+    //logical AND
+    //logical OR
+    //logical NOT
+
+
+//3. CONDITIONAL STATEMENT
+    //If-else
+        //password condition
+        //years of experience
+        //pwd discount
+        //withdrawal amount
+
+
+    //switch-case
+        //tier- ss,s,a,b,c,d
+        //transporation fare/mode = jeep 13, bus 15, aircon-20, minibus-12, grab-200,lrt-15
+        
+
+//4. LOOPS AND ITERATION
+    //for loops
+        //months loop
+        //print loop
+        //process loop
+        
+
+    
+    //while loop
+        
+
+
+//5. FUNCTIONS
+    //basic function
+    //parameterize function
+    //using return and function assignment
+
+    //examples
+    //additional function
+
+    //create a function that accepts distance(km) as an input, where you calculate the grab fare distance for 10peso per km. The starting fare begins at 200.
+
+    //create a function that accepts two input, the amount tendered(amount due) and a boolean for pwd, compute for the net amount, and return the computed value.
+
+    //create a function that accepts first name and last name, and generate an automatic email for it, using the format 
+    //(first letter of the firstname).(lastname)@sdca.edu.ph
+    //f.barbon@sdca.edu.ph
+
+
+//Objects
+    //Object structure
+    //accessing objects
+    //modifying objects
+    //adding keys
+    //looping objects
+    //looping array of objects
+    
